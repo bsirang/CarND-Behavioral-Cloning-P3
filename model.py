204a49593976feb5e713a4b21fed52ea1155c478
Behrooze Sirang <behrooze@gmail.com>
2020-03-29 23:52:58 -0700

    Reduce epochs

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ def read_image(file):
     #image = image.reshape(image.shape + (1,))
     #return image
     return cv2.imread(file)
-    
+
 
 def input_batch_generator(samples, datadir, batch_size, use_flipped=True, use_lr=True):
     num_samples = len(samples)
@@ -30,7 +30,7 @@ def input_batch_generator(samples, datadir, batch_size, use_flipped=True, use_lr
         divisor += 1
     if use_lr == True:
         divisor += 2
-        
+
     batch_size = int(batch_size / divisor)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -39,12 +39,12 @@ def input_batch_generator(samples, datadir, batch_size, use_flipped=True, use_lr
 
             images = []
             angles = []
-            for batch_sample in batch_samples:          
+            for batch_sample in batch_samples:
                 center_image = read_image(datadir + '/IMG/' + batch_sample[0].split('/')[-1])
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
-                
+
                 if use_lr == True:
                     left_image = read_image(datadir + '/IMG/' + batch_sample[1].split('/')[-1])
                     right_image = read_image(datadir + '/IMG/' + batch_sample[2].split('/')[-1])
@@ -61,7 +61,7 @@ def input_batch_generator(samples, datadir, batch_size, use_flipped=True, use_lr
                     center_angle_flipped = -center_angle
                     images.append(center_flipped)
                     angles.append(center_angle_flipped)
-                    
+
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -76,7 +76,7 @@ def load_csv(filepath):
             if count > 0: # Ignore first row (column header)
                 samples.append(line)
             count += 1
-            
+
     print("Loaded {} samples from {}".format(len(samples), filepath))
     return samples
 
@@ -86,7 +86,7 @@ def create_train_and_validation_samples(filepath, test_size=0.2):
     train_samples, validation_samples = train_test_split(samples, test_size=test_size)
     print("Split samples into {} training and {} validation samples".format(len(train_samples), len(validation_samples)))
     return train_samples, validation_samples
-            
+
 def create_nvidia_e2e_model():
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
@@ -106,18 +106,16 @@ def create_nvidia_e2e_model():
     model.compile(loss='mse', optimizer='adam')
     return model
 
-def define_training_and_validation_data(model, datadir, batch_size, epochs=20):
-    train_samples, validation_samples = create_train_and_validation_samples(datadir + '/driving_log.csv')    
+def define_training_and_validation_data(model, datadir, batch_size, epochs=11):
+    train_samples, validation_samples = create_train_and_validation_samples(datadir + '/driving_log.csv')
     train_generator = input_batch_generator(train_samples, datadir, batch_size=batch_size)
     validation_generator = input_batch_generator(validation_samples, datadir, batch_size=batch_size)
     history_object = model.fit_generator(train_generator, steps_per_epoch=ceil(len(train_samples)/batch_size),
                         validation_data=validation_generator,
                         validation_steps=ceil(len(validation_samples)/batch_size),
                         epochs=epochs, verbose=1)
-    
+
     return model, history_object
-    
+
 model, history_object = define_training_and_validation_data(create_nvidia_e2e_model(), '/home/workspace/CarND-Behavioral-Cloning-P3/training/data', 32)
 model.save('model.h5')
-
-
