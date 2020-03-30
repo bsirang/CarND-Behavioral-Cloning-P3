dda494b6cdc6ef87dc4b0338819b6141baa9887a
Behrooze Sirang <behrooze@gmail.com>
2020-03-30 00:19:14 +0000

    Best performance so far

--- model.py
+++ model.py
@@ -8,17 +8,30 @@ from numpy.random import shuffle
 
 
 from keras.models import Sequential, Model
-from keras.layers import Cropping2D, Conv2D, Flatten, Dense, Lambda
+from keras.layers import Cropping2D, Conv2D, Flatten, Dense, Lambda, Dropout
 
 import sklearn
 from sklearn.model_selection import train_test_split
 
-def input_batch_generator(samples, datadir, batch_size):
+def read_image(file):
+    #image = cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY)
+    #image = image.reshape(image.shape + (1,))
+    #return image
+    return cv2.imread(file)
+    
+
+def input_batch_generator(samples, datadir, batch_size, use_flipped=True, use_lr=True):
     num_samples = len(samples)
-    # We append an augmented (flipped) image for every input
-    # We'll cut the batch size in half here so that the effective batch size
-    # remains the same
-    batch_size = int(batch_size / 2)
+    # For every "sample" we actually have four training inputs
+    # Left, center, and right images plus a left/right mirrored version of the center
+    # So we divide the user-given batch size by four
+    divisor = 1
+    if use_flipped == True:
+        divisor += 1
+    if use_lr == True:
+        divisor += 2
+        
+    batch_size = int(batch_size / divisor)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
@@ -26,18 +39,29 @@ def input_batch_generator(samples, datadir, batch_size):
 
             images = []
             angles = []
-            for batch_sample in batch_samples:
-                name = datadir + '/IMG/' + batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
+            for batch_sample in batch_samples:          
+                center_image = read_image(datadir + '/IMG/' + batch_sample[0].split('/')[-1])
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
-                center_flipped = np.fliplr(center_image)
-                center_angle_flipped = -center_angle
-                images.append(center_flipped)
-                angles.append(center_angle_flipped)
+                
+                if use_lr == True:
+                    left_image = read_image(datadir + '/IMG/' + batch_sample[1].split('/')[-1])
+                    right_image = read_image(datadir + '/IMG/' + batch_sample[2].split('/')[-1])
+                    correction = 0.1 # based on emperical tuning
+                    left_angle = center_angle + correction
+                    right_angle = center_angle - correction
+                    images.append(left_image)
+                    images.append(right_image)
+                    angles.append(left_angle)
+                    angles.append(right_angle)
 
-            # trim image to only see section with road
+                if use_flipped == True:
+                    center_flipped = np.fliplr(center_image)
+                    center_angle_flipped = -center_angle
+                    images.append(center_flipped)
+                    angles.append(center_angle_flipped)
+                    
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -66,21 +90,23 @@ def create_train_and_validation_samples(filepath, test_size=0.2):
 def create_nvidia_e2e_model():
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Cropping2D(cropping=((70,25), (0,0)))) # Trim images to see only section of road
     model.add(Conv2D(24, (5, 5), strides=(2,2), activation='relu'))
     model.add(Conv2D(36, (5, 5), strides=(2,2), activation='relu'))
     model.add(Conv2D(48, (5, 5), strides=(2,2), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Flatten())
+    model.add(Dropout(0.2))
     model.add(Dense(100))
+    model.add(Dropout(0.2))
     model.add(Dense(50))
     model.add(Dense(10))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
     return model
 
-def define_training_and_validation_data(model, datadir, batch_size, epochs=5):
+def define_training_and_validation_data(model, datadir, batch_size, epochs=20):
     train_samples, validation_samples = create_train_and_validation_samples(datadir + '/driving_log.csv')    
     train_generator = input_batch_generator(train_samples, datadir, batch_size=batch_size)
     validation_generator = input_batch_generator(validation_samples, datadir, batch_size=batch_size)
@@ -91,8 +117,6 @@ def define_training_and_validation_data(model, datadir, batch_size, epochs=5):
     
     return model, history_object
     
-print("Hello")
-
 model, history_object = define_training_and_validation_data(create_nvidia_e2e_model(), '/home/workspace/CarND-Behavioral-Cloning-P3/training/data', 32)
 model.save('model.h5')
 
